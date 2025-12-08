390c6c7cd6c242ed46ea1a1ee396e8fedd8b9370
gubard <maksimovsergey08@gmail.com>
2025-12-08 15:31:57 +0200

    Add `WebApplicationBuilderExtension` to streamline Zeus app setup and enhance service configuration.

--- Zeus/Helpers/ServiceCollectionExtension.cs
+++ Zeus/Helpers/ServiceCollectionExtension.cs
@@ -1,18 +1,48 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Gaia.Helpers;
+using Gaia.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Nestor.Db.Sqlite;
 
 namespace Zeus.Helpers;
 
 public static class ServiceCollectionExtension
 {
-    public static IServiceCollection AddJwtAuthentication(
-        this IServiceCollection serviceCollection,
-        IConfiguration configuration
-    )
+    extension(IServiceCollection serviceCollection)
     {
-        serviceCollection.AddAuthentication(x => x.SetJwtBearerDefaults())
-           .AddJwtBearer(x => x.SetJwtOptions(configuration));
+        public IServiceCollection AddJwtAuthentication(IConfiguration configuration)
+        {
+            serviceCollection.AddAuthentication(x => x.SetJwtBearerDefaults())
+               .AddJwtBearer(x => x.SetJwtOptions(configuration));
 
-        return serviceCollection;
+            return serviceCollection;
+        }
+
+        public IServiceCollection AddZeusDbContext(string name)
+        {
+            return serviceCollection.AddDbContext<DbContext, SqliteNestorDbContext>((sp, options) =>
+            {
+                var httpContextAccessor = sp.GetRequiredService<IHttpContextAccessor>();
+                var userId = httpContextAccessor.HttpContext.ThrowIfNull().GetUserId();
+                var dataSourceFile = sp.GetRequiredService<IStorageService>().GetDbDirectory().Combine(name).ToFile($"{userId}.db");
+                options.UseSqlite($"Data Source={dataSourceFile}", x => x.MigrationsAssembly(typeof(SqliteNestorDbContext).Assembly));
+
+                if (dataSourceFile.Exists)
+                {
+                    return;
+                }
+
+                if (dataSourceFile.Directory?.Exists != true)
+                {
+                    dataSourceFile.Directory?.Create();
+                }
+
+                using var context = new SqliteNestorDbContext(options.Options);
+                context.Database.Migrate();
+            });
+        }
     }
+
 }
---
+++ Zeus/Helpers/WebApplicationBuilderExtension.cs
@@ -0,0 +1,65 @@
+﻿using Gaia.Helpers;
+using Gaia.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Zeus.Services;
+
+namespace Zeus.Helpers;
+
+public static class WebApplicationBuilderExtension
+{
+    public static async ValueTask
+        RunZeusApp<TServiceInterface, TService, TGetRequest, TPostRequest,
+            TGetResponse, TPostResponse>(
+            this WebApplicationBuilder builder, string name)
+        where TServiceInterface : class, IService<TGetRequest, TPostRequest,
+            TGetResponse, TPostResponse>
+        where TService : class, TServiceInterface
+        where TGetResponse : IValidationErrors, new()
+        where TPostResponse : IValidationErrors, new()
+    {
+        builder.Services.AddOpenApi();
+        builder.Services.AddAuthorization();
+        builder.Services.AddHttpContextAccessor();
+        builder.Services.AddJwtAuthentication(builder.Configuration);
+        builder.Services.AddTransient<IStorageService, StorageService>();
+        builder.Services.AddTransient<TServiceInterface, TService>();
+        builder.Services.AddTransient<IDbMigrator, DbMigrator>(sp =>
+            new(sp.GetRequiredService<IStorageService>().GetDbDirectory()
+               .Combine(name)));
+        builder.Services.AddZeusDbContext(name);
+
+        var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapPost(RouteHelper.Get,
+                (TGetRequest request,
+                        TServiceInterface authenticationService,
+                        CancellationToken ct) =>
+                    authenticationService.GetAsync(request, ct))
+           .RequireAuthorization()
+           .WithName(RouteHelper.GetName);
+
+        app.MapPost(RouteHelper.Post,
+                (TPostRequest request,
+                        TServiceInterface authenticationService,
+                        CancellationToken ct) =>
+                    authenticationService.PostAsync(request, ct))
+           .RequireAuthorization()
+           .WithName(RouteHelper.PostName);
+
+        await app.Services.GetRequiredService<IDbMigrator>()
+           .MigrateAsync(CancellationToken.None);
+        await app.RunAsync();
+    }
+}
