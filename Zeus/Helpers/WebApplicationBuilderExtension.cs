db0ae61b469a6b4bef34cef35ee36443804e1b47
gubard <maksimovsergey08@gmail.com>
2026-09-16 15:23:42 +0300

    Remove unused `Microsoft.Extensions.Configuration` import in `WebApplicationBuilderExtension`.

--- Zeus/Helpers/WebApplicationBuilderExtension.cs
+++ Zeus/Helpers/WebApplicationBuilderExtension.cs
@@ -4,7 +4,6 @@ using Gaia.Models;
 using Gaia.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Nestor.Db.LiteDb.Services;
